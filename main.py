db2304aefa2040b29e7e18f082a6b9ffa535e763
tony430 <tonychang430@hotmail.com>
2021-06-08 16:47:58 +0800

    Added speech recognition method

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sounddevice as sd
+import speech_recognition as sr
+
 from scipy.io.wavfile import write
+
 
 def record(soundpath):
     fs = 44100  # free-air resonant frequency
@@ -9,12 +12,27 @@ def record(soundpath):
     sd.wait() # wait until the recording is done
     write(soundpath,fs, myrecording)
 
+def convertSpeech(soundpath):
+    r = sr.Recognizer()
+    with sr.AudioFile(soundpath) as source:
+        audio_text = r.listen(source)
+
+    # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+    try:
+        # using google speech recognition
+        text = r.recognize_google(audio_text)
+        print('Converting audio transcripts into text ...')
+        print(text)
+
+    except:
+        print('Sorry.. run again...')
+
 # main method
 if __name__ == '__main__':
 
-    soundpath = 'output.wav'
+    soundpath = 'output.mp3'
     record(soundpath)
-
+    convertSpeech(soundpath)
 
 
 
