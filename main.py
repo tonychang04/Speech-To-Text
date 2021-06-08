2626412b8fc064398acb7f627ced77830d1b8810
tony430 <tonychang430@hotmail.com>
2021-06-08 15:49:31 +0800

    Fixed channel bug

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 
-def record():
+def record(soundpath):
     fs = 44100  # free-air resonant frequency
     seconds = 5  # first use prefixed time
 
-    myrecording = sd.rec(int(seconds * fs), samplerate = fs, channels = 2)
+    myrecording = sd.rec(int(seconds * fs), samplerate = fs, channels = 1)
     sd.wait() # wait until the recording is done
-    write('output.wav',fs, myrecording )
+    write(soundpath,fs, myrecording)
 
 # main method
 if __name__ == '__main__':
-    record()
+
+    soundpath = 'output.wav'
+    record(soundpath)
 
 
 
